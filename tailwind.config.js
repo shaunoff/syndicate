0a619a901f531866fcfe52bcb01a6f49dbee4b01
shaun Hutch <shaun@promoboxx.com>
2021-06-19 18:46:58 -0400

    styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ const themeWrapper = require("@shaunoff-ui/components/config")
 
 module.exports = themeWrapper({
   mode: "jit",
-  purge: [
-    "{pages,app}/**/*.{js,jsx,ts,tsx}",
-    // "node_modules/@shaunoff-ui/components/src/theme/defaultTheme.ts",
-    // "./node_modules/@shaunoff-ui/components/dist/theme/index.d.ts",
-  ],
+  purge: ["{pages,app}/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
